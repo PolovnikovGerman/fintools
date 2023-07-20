2b82b0973ac9cc5ca142819eba2ab431d3a0e790
German <polovnikov.g@gmail.com>
2023-07-20 10:58:10 +0300

    Export clay and previews

--- export_lift.php
+++ export_lift.php
@@ -4,6 +4,7 @@ include('model/mysql.php');
 $username='develop';
 $password='mJWFFZ5mFhjy';
 $URL='http://lifttest.stressballs.com/export/claypreview_export';
+$authtoken = base64_encode($username.':'.$password);
 
 $obj = new db();
 $qry = "select id, order_number, doc_type, doc_link, doc_name from lift_exports where managed=0 limit 100";
@@ -16,17 +17,27 @@ while($data = $obj->fetch($res) ) {
         'doc_name' => $data['doc_name'],
     );
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$URL);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-    curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-    $result=curl_exec ($ch);
-    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
-    curl_close ($ch);
-    echo 'Curl send '.$data['id'].' Status - '.$status_code.PHP_EOL;
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $URL,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $params,
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Basic '.$authtoken
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+    var_dump($status_code);
     $updsql = "update lift_exports set managed=1 where id=".$data['id'];
     $resupd = $obj->query($updsql);
 }
