aca611344a3657801f3243df076dbf7469133a6d
German <polovnikov.g@gmail.com>
2023-07-20 11:20:19 +0300

    Export clay and previews

--- export_lift.php
+++ export_lift.php
@@ -5,9 +5,12 @@ $username='develop';
 $password='mJWFFZ5mFhjy';
 $URL='http://lifttest.stressballs.com/export/claypreview_export';
 $authtoken = base64_encode($username.':'.$password);
+$headers = array(
+    'Authorization: Basic '.$authtoken,
+);
 
 $obj = new db();
-$qry = "select id, order_number, doc_type, doc_link, doc_name from lift_exports where managed=0 limit 100";
+$qry = "select id, order_number, doc_type, doc_link, doc_name from lift_exports where managed=0 limit 10";
 $res = $obj->query($qry);
 while($data = $obj->fetch($res) ) {
     $params = array(
@@ -16,30 +19,27 @@ while($data = $obj->fetch($res) ) {
         'doc_link' => $data['doc_link'],
         'doc_name' => $data['doc_name'],
     );
+    try {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $URL);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        $apiResponse=curl_exec ($ch);
+        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
+        curl_close ($ch);
+        if ($status_code==200) {
+            echo 'Success '.PHP_EOL;
+            $updsql = "update lift_exports set managed=1 where id=".$data['id'];
+            $resupd = $obj->query($updsql);
+        }
+    } catch (Exception $e) {
+        echo 'Error '.PHP_EOL;
+        var_dump($e);
+        die();
+    }
 
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $URL,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => $params,
-        CURLOPT_HTTPHEADER => array(
-            'Authorization: Basic '.$authtoken
-        ),
-    ));
-
-    $response = curl_exec($curl);
-    $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    curl_close($curl);
-    var_dump($status_code);
-    $updsql = "update lift_exports set managed=1 where id=".$data['id'];
-    $resupd = $obj->query($updsql);
 }
 $endtime = time();
 $period = ($endtime - $starttime);
