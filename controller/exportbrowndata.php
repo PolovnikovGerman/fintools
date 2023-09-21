c16a3721ad7f3b065f38e2b86bf9970b1e8de3da
German <polovnikov.g@gmail.com>
2023-09-21 12:41:08 +0300

    Migration to php v.8.2

--- controller/exportbrowndata.php
+++ controller/exportbrowndata.php
@@ -7,16 +7,17 @@ include('../includes/utility_functions.php');
 $out = array('result' => 0, 'error' => 'Order Not Found');
 $postdata = $_POST;
 // Get Order bi ID
-$dat = get_orderart($postdata['af_order_id']);
-if (array_key_exists('af_order_id', $dat)) {
-    if ($postdata['sync']=='data') {
-        // Order Found
-        $updres=update_orderart($postdata, $dat['af_notes']);        
-        $out['error']='';
-        $out['result']=1;
-    } else {
-        // Save Documents
-//        $fh = fopen('../log/exportdata.log', 'a+');        
+if (isset($postdata['af_order_id'])) {
+    $dat = get_orderart($postdata['af_order_id']);
+    if (array_key_exists('af_order_id', $dat)) {
+        if ($postdata['sync']=='data') {
+            // Order Found
+            $updres=update_orderart($postdata, $dat['af_notes']);
+            $out['error']='';
+            $out['result']=1;
+        } else {
+            // Save Documents
+//        $fh = fopen('../log/exportdata.log', 'a+');
 //        foreach ($postdata as $key=>$val) {
 //            $msg = date('d.m.Y H:i:s') . 'POST KEY '.$key.' Value '.$val.PHP_EOL;
 //            fwrite($fh, $msg);
@@ -24,9 +25,10 @@ if (array_key_exists('af_order_id', $dat)) {
 //        fclose($fh);
 //        $out['result']=0;
 //        $out['error']='Test';
-        $updres=update_orderartdoc($postdata);
-        $out['result']=$updres['result'];
-        $out['error']=$updres['error'];
+            $updres=update_orderartdoc($postdata);
+            $out['result']=$updres['result'];
+            $out['error']=$updres['error'];
+        }
     }
 }
 echo json_encode($out);
