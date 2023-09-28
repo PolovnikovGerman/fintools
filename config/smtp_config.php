0b7b2bb64acd81921aef57d65df06cad270938eb
German <polovnikov.g@gmail.com>
2023-09-28 11:54:01 +0300

    Email Settings

--- config/smtp_config.php
+++ config/smtp_config.php
@@ -1,1 +1,7 @@
 <?php
+define('SMTP_SERVER');
+define('SMTP_SECURE');
+define('SMTP_PORT');
+define('SMTP_PORT');
+define('SMTP_USER');
+define('SMTP_PASSWORD');
--- view/check_email.php
+++ view/check_email.php
@@ -26,13 +26,20 @@ $data2 = array();
 $data2['v_email'] = 'to_german@yahoo.com';
 $data2['v_additional_email'] = 'german.polovnikov@bluetrack.com';
 $data2['v_name'] = 'ChemLite';
-$oid = 43519;
+$oid = 63238;
 $chpo = 'A';
 $par=array($data2['v_name'],'BT'.$oid.$chpo);
 $msg=emailTemplate('po',$par);
 $mail->Body = $msg;
 $save_name = "../docs/art_af_fl_uploads/poart/BLUETRACK_PO_BT".$oid.$chpo.".pdf";
-$mail->addAttachment($save_name, 'BLUETRACK_PO_BT43519A');         //Add attachments
+$mail->addAttachment($save_name, "BLUETRACK_PO_BT".$oid.$chpo.".pdf");         //Add attachments
+$qry = "select att_path, att_name from af_attach where att_ref = ".$_POST['oid']." and att_type = 'art'";
+$obj = new db();
+$res = $obj->query($qry);
+while($data = $obj->fetch($res)){
+    $mail->addAttachment($data['att_path'], $data['att_name']);         //Add attachments
+}
+
 if(!$mail->send()) {
     echo 'Error during send Email'.PHP_EOL;
 } else {
