dae4d9c535836a4f8dde10c2099a235bc9a31c97
German <polovnikov.g@gmail.com>
2023-10-13 11:03:34 +0300

    Email via SMTP

--- controller/poart_sendmail.php
+++ controller/poart_sendmail.php
@@ -9,7 +9,10 @@ while ($data = $obj->fetch($res)) {
         $qryupd = "update email_queue set send=1, sendtime='".date('Y-m-d H:i:s')."' where id=".$data['id'];
         $resupd = $obj->query($qryupd);
         // Prepare email
-        $attachs = json_decode($data['email_attach'], true);
+        $attachs = array();
+        if (!empty($data['email_attach'])) {
+            $attachs = json_decode($data['email_attach'], true);
+        }
         $sendres = send_email_docs($data['email_to'], $data['email_subj'], $data['email_body'], $attachs, $data['email_cc']);
         $qryupd1 = "update email_queue set send_result=".$sendres." where id=".$data['id'];
         $resupd1 = $obj->query($qryupd1);
