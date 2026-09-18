4ed0d40f273ccc8fe9cdfac2e7a933b4e5f2e8e3
German Polovnikov <polovnikov.g@gmail.com>
2026-09-19 02:05:44 +0300

    Add export PO data

--- controller/exportpodata.php
+++ controller/exportpodata.php
@@ -3,7 +3,7 @@ $out = array('result' => 0, 'error' => 'Clay / Previews Not Found');
 include_once('../model/mysql.php');
 $obj = new db();
 $results = array();
-$qry = 'select * from lift_exportpo where managed=0 order by id limit 100';
+$qry = 'select * from lift_exportpo where managed=0 order by id limit 10';
 $res = $obj->query($qry);
 while($data = $obj->fetch($res)){
     $methods = array();
